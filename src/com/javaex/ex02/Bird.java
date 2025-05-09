c4dedaf1b73549bd9a204449d5826480e662e465
gadongmin <dmkim777@naver.com>
2025-05-09 17:40:31 +0900

    20250509(1)

--- src/com/javaex/ex02/Bird.java
+++ src/com/javaex/ex02/Bird.java
@@ -1,8 +1,31 @@
 package com.javaex.ex02;
 
 public abstract class Bird {
-    private String name;
+	// 필드
+	private String name;
 
+	// 생성자
+	public Bird() {
+	}
 
+	public Bird(String name) {
+		this.name = name;
+	}
 
+	// 메소드gs
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// 메소드 일반
+	@Override
+	public abstract String toString();
+
+    public abstract void fly();
+    public abstract void sing();
+    public abstract void showName();
 }
